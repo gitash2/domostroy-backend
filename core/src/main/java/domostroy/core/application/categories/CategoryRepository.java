1bbcb63913eb86398e60d5b9b36abed18a0fcd5f
gitash2 <shapor.art@yandex.ru>
2025-05-03 22:50:47 +0300

    [DOM-283] add category methods
    Took 1 minute

--- core/src/main/java/domostroy/core/application/categories/CategoryRepository.java
+++ core/src/main/java/domostroy/core/application/categories/CategoryRepository.java
@@ -1,9 +1,15 @@
 package domostroy.core.application.categories;
 
 import domostroy.aggregates.category.Category;
+import domostroy.core.adapters.adaptersOutput.categories.projections.CategoryProjection;
+
+import java.util.List;
 
 public interface CategoryRepository {
     Category save(Category aggregate);
     Category findByName(String name);
     Category findById(Integer id);
+    List<CategoryProjection> findAll();
+    Long count();
+    List<CategoryProjection> saveAll(List<CategoryProjection> categories);
 }
