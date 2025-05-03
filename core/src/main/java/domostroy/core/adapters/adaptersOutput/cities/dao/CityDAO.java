87298bebf79ac8039c1f5ab07554450a22657b3e
gitash2 <shapor.art@yandex.ru>
2025-05-03 22:51:00 +0300

    [DOM-283] add city entity methods
    Took 13 seconds

--- core/src/main/java/domostroy/core/adapters/adaptersOutput/cities/dao/CityDAO.java
+++ core/src/main/java/domostroy/core/adapters/adaptersOutput/cities/dao/CityDAO.java
@@ -2,7 +2,27 @@ package domostroy.core.adapters.adaptersOutput.cities.dao;
 
 import domostroy.core.adapters.adaptersOutput.cities.projections.CityProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CityDAO extends JpaRepository<CityProjection, Integer> {
+
+    @Query("""
+        select c
+        from CityProjection c
+        order by c.id asc
+        limit 5
+    """)
+    List<CityProjection> findPopularCities();
+
+
+    @Query("""
+        select c
+        from CityProjection c
+        where lower(c.name)
+        like lower(concat('%', :city, '%'))
+    """)
+    List<CityProjection> findByMatch(String city);
 
 }
