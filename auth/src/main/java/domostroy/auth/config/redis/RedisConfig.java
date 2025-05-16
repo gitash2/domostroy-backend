78f5e59504d3f32a53f81142c476048c07faea3b
gitash2 <shapor.art@yandex.ru>
2025-05-16 19:48:15 +0300

    [dev] test
    Took 5 hours 15 minutes

--- auth/src/main/java/domostroy/auth/config/redis/RedisConfig.java
+++ auth/src/main/java/domostroy/auth/config/redis/RedisConfig.java
@@ -1,21 +1,35 @@
 package domostroy.auth.config.redis;
 
 import domostroy.auth.dto.VerificationData;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableConfigurationProperties(RedisConfig.DataRedisProps.class)
 public class RedisConfig {
 
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+    public RedisConnectionFactory redisConnectionFactory(DataRedisProps props) {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(props.getHost(), props.getPort()));
+    }
+
+    @ConfigurationProperties(prefix = "spring.data.redis")
+    public static class DataRedisProps {
+        private String host;
+        private int port;
+        public String getHost() { return host; }
+        public void setHost(String host) { this.host = host; }
+        public int getPort() { return port; }
+        public void setPort(int port) { this.port = port; }
     }
 
     @Bean
